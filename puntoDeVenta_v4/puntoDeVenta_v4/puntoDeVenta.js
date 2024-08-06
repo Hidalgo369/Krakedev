c936b7fc6516b9a4c84f603fa8dd84a8210033ca
Hidalgo369 <ljhidalboy@gmail.com>
2024-08-06 18:52:26 -0500

    Prueba exitosa de agregar y mostrar en el label resumen, el valor a cancelar

--- puntoDeVenta_v4/puntoDeVenta_v4/puntoDeVenta.js
+++ puntoDeVenta_v4/puntoDeVenta_v4/puntoDeVenta.js
@@ -93,7 +93,9 @@ calcularValorTotal = function () {
        */
             
     //12. Mostrar un resumen en el componente lblResumen, si no existe debe agregarlo
-   
+    let resumen="Valor a pagar por "+cantidad+" "+nombreProducto+" con "+porcentajeDescuento+"% de descuento: USD "+valorTotal;
+    let cmpResumen=document.getElementById("lblResumen");
+    cmpResumen.innerText=resumen;
     /*
         Ejemplo: 
             Valor a pagar por 20 cerveza corona con 10% de descuento: USD 48.75
