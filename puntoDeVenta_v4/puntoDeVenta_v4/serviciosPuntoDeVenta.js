cb3b9385bb0e2eac061824b97a58e77093c9347a
Hidalgo369 <ljhidalboy@gmail.com>
2024-08-06 17:47:15 -0500

    Prueba exitosa de subtotal en el archivo PuntoDeVenta.js

---
+++ puntoDeVenta_v4/puntoDeVenta_v4/serviciosPuntoDeVenta.js
@@ -0,0 +1,19 @@
+calcularValorDescuento=function(monto,porcentajeDescuento){
+    let valorDescuento=(monto*porcentajeDescuento)/100;
+    return valorDescuento;
+}
+
+calcularIva=function(monto){
+    let valorIva=monto*0.12;
+    return valorIva;
+}
+
+calcularSubtotal=function(precio,cantidad){
+    let total=precio*cantidad;
+    return total;
+}
+
+calcularTotal=function(subtotal,descuento,iva){
+    let totalPagar=(subtotal-descuento)+iva;
+    return totalPagar;
+}
