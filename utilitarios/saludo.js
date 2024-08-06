9a76a28638c12a5fd635b56f11bc36e0d3cd640d
Hidalgo369 <ljhidalboy@gmail.com>
2024-08-06 16:38:34 -0500

    Se separan las funciones al archivo utilitarios.js

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -5,37 +5,6 @@ saludar=function(){
     let estatura=recuperarFloat("txtEstatura");
     let mensajeBienvenida="Bienvenido "+nombre+" "+apellido;
     mostrarTexto("lblResultado",mensajeBienvenida);
-    mostrarImagen("imgSaludo","./imagenes/goku.gif")
-}
-
-mostrarImagen=function(idComponente,rutaImagen){
-    let componente;
-    componente=document.getElementById(idComponente);
-    componente.src=rutaImagen;
-}
-
-mostrarTexto=function(idComponente,mensaje){
-    let componente;
-    componente=document.getElementById(idComponente);
-    componente.innerText=mensaje;
-}
-
-recuperarTexto=function(idComponente){
-    let componente;
-    let valorIngresado;
-    componente=document.getElementById(idComponente);
-    valorIngresado=componente.value;
-    return valorIngresado;
-}
-
-recuperarInt=function(idComponente){
-    let valorCaja= recuperarTexto(idComponente);
-    let valorEntero= parseInt(valorCaja);
-    return valorEntero;
-}
-
-recuperarFloat=function(idComponente){
-    let valorCaja= recuperarTexto(idComponente);
-    let valorEntero= parseFloat(valorCaja);
-    return valorEntero;
+    mostrarImagen("imgSaludo","./imagenes/goku.gif");
+    mostrarTextoEnCaja("txtNombre","")
 }
