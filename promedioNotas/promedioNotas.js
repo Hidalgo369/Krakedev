a30c1ac9a9e30efae2a2475b8b01a868c93379fc
Hidalgo369 <ljhidalboy@gmail.com>
2024-08-07 15:21:10 -0500

    Funciona todo bien el ejercicio de calcular notas

---
+++ promedioNotas/promedioNotas.js
@@ -0,0 +1,9 @@
+calcularPromedioNotas=function(){
+    let nota1=recuperarFloat("caja1");
+    let nota2=recuperarFloat("caja2");
+    let nota3=recuperarFloat("caja3");
+    let promedio=calcularPromedio(nota1,nota2,nota3);
+    let mostrarPromedio=promedio.toFixed(2);
+
+    mostrarTexto("lblResultado",mostrarPromedio);
+}
