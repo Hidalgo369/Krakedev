f01b02e860c5f3890c5d391b0406653436597aff
Hidalgo369 <ljhidalboy@gmail.com>
2024-08-06 19:41:05 -0500

    Función de limpiar completada, reto terminado

--- puntoDeVenta_v4/puntoDeVenta_v4/puntoDeVenta.js
+++ puntoDeVenta_v4/puntoDeVenta_v4/puntoDeVenta.js
@@ -1,18 +1,13 @@
 calcularValorTotal = function () {
     //variables para recuperar los valores de las cajas de texto
-    let cmpNombreProducto=document.getElementById("txtProducto");
-    let cmpPrecioProducto=document.getElementById("txtPrecio");
-    let cmpCantidad=document.getElementById("txtCantidad"); 
-    let cmpPorcentajeDescuento=document.getElementById("txtPorcentajeDescuento");
-
     //1. Recuperar el nombre del producto como String
-    let nombreProducto=cmpNombreProducto.value;
+    let nombreProducto=recuperarTexto("txtProducto");
     //2. Recuperar el precio como float
-    let precioProducto=parseFloat(cmpPrecioProducto.value); // SE UTILIZA PARA RECUPERAR EL PRECIO COMO FLOAT
+    let precioProducto=recuperarFloat("txtPrecio"); // SE UTILIZA PARA RECUPERAR EL PRECIO COMO FLOAT
     //3. Recuperar cantidad como int
-    let cantidad=parseInt(cmpCantidad.value); // SE UTILIZA PARA RECUPERAR LA CANTIDAD COMO INT
+    let cantidad=recuperarInt("txtCantidad"); // SE UTILIZA PARA RECUPERAR LA CANTIDAD COMO INT
     //4. Recuperar el porcentaje de descuento como int
-    let porcentajeDescuento=parseInt(cmpPorcentajeDescuento.value);
+    let porcentajeDescuento=recuperarInt("txtPorcentajeDescuento");
 
     //variables para almacenar los retornos de las funciones
     let valorSubtotal;
@@ -103,11 +98,23 @@ calcularValorTotal = function () {
     */
 
 }
-limpiar = function () {
+limpiar=function(){
+    mostrarTextoEnCaja("txtProducto","");
+    mostrarTextoEnCaja("txtPrecio","");
+    mostrarTextoEnCaja("txtCantidad",""); 
+    mostrarTextoEnCaja("txtPorcentajeDescuento","");
+
+    mostrarTexto("lblSubtotal","0.0");
+    mostrarTexto("lblDescuento","0.0");
+    mostrarTexto("lblValorIVA","0.0");
+    mostrarTexto("lblTotal","0.0");
+    mostrarTexto("lblResumen","");
     /*
         Dejar todas las cajas de texto con el valor cadena vacía, 0 ó 0.0 según el tipo de dato
         Dejar todos los textos de los montos con el valor 0.0
+
         Si funciona, hacer un commit
      */
+
 }
 /* SI TODO FUNCIONA, HACER UN PUSH */
