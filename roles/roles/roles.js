059a603a337dae679a13d306240f4df5557b675c
Hidalgo369 <ljhidalboy@gmail.com>
2024-08-14 15:02:59 -0500

    Todo funciona bien, mostrarEmpleados en tabla

--- roles/roles/roles.js
+++ roles/roles/roles.js
@@ -1,12 +1,14 @@
 let empleados = [
     { cedula: "1714616123", nombre: "John", apellido: "Cena", sueldo: 500.0 },
-    { cedula: "0914632123", nombre: "Luisa", apellido: "Gonzalez", sueldo: 900.0 }
+    { cedula: "0914632123", nombre: "Luisa", apellido: "Gonzalez", sueldo: 900.0 },
+    { cedula: "0987654321", nombre: "Daniel", apellido: "Pintado", sueldo: 500.0 }
 ]
 
 mostrarOpcionEmpleado = function () {
     mostrarComponente("divEmpleado");
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
+    mostrarEmpleados();
 }
 
 mostrarOpcionRol = function () {
@@ -19,4 +21,28 @@ mostrarOpcionResumen = function () {
     ocultarComponente("divEmpleado");
     ocultarComponente("divRol");
     mostrarComponente("divResumen");
+}
+
+mostrarEmpleados = function () {
+    mostrarComponente("tablaEmpleados");
+    let cmpTabla = document.getElementById("tablaEmpleados");
+    let contenidoTabla = "<table><tr>" +
+        "<th>CEDULA</th>" +
+        "<th>NOMBRE</th>" +
+        "<th>APELLIDO</th>" +
+        "<th>SUELDO</th>" +
+        "</tr>";
+    let elementoCliente;
+    for (let i = 0; i < empleados.length; i++) {
+        elementoCliente = empleados[i];
+        contenidoTabla +=
+            "<tr><td>" + elementoCliente.cedula + "</td>"
+            + "<td>" + elementoCliente.nombre + "</td>"
+            + "<td>" + elementoCliente.apellido + "</td>"
+            + "<td>" + elementoCliente.sueldo + "</td>"
+            + "</tr>";
+
+    }
+    contenidoTabla += "</table>";
+    cmpTabla.innerHTML = contenidoTabla;
 }
